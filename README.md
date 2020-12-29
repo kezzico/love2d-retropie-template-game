14421f2aa6af260b553c606c6b15434a012d6857
Lee Irvine <lee@kezzi.co>
2020-12-28 17:24:03 -0800

    sample files, readme, explainer text

---
+++ README.md
@@ -0,0 +1,13 @@
+# TEMPLATE GAME
+
+A basic game template useful for Love2d games running on a Retropie. Built for games using a Ps3, Ps4, or Xbox controller. Supports multiple players.
+
+Includes
+  Start + select button to exit the game.
+
+  Multiple controllers / multiple players support
+
+  Code samples
+
+	Lua object oriented programming
+
--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ local players = { }
 
 local music = nil
 
+local font = nil
+
 function love.load()
 	require 'utils'
 
@@ -11,7 +13,11 @@ function love.load()
 
 	require 'player'
 
---	music = love.audio.newSource('sounds/mario-overworld.ogg', "stream")
+	font = love.graphics.newFont("fonts/joystix.ttf", 25)
+
+	love.graphics.setFont(font)
+
+--	music = love.audio.newSource("sounds/mario-overworld.ogg", "stream")
 
 --	music:setLooping(true)
 
@@ -70,17 +76,35 @@ function love.gamepadreleased(joystick, button)
 end
 
 function love.draw()
+	local screenWidth = love.graphics.getWidth( )
+	local screenHeight = love.graphics.getHeight( )
+
 	love.graphics.clear( 91/255, 151/255, 243/255, 1.0)
 
+	-- if there are no players
+	if #players == 0 then
+		love.graphics.setColor(1, 0, 0, 1)
+		love.graphics.printf(
+			"TEMPLATE GAME", 
+			0, screenHeight / 2.0, 
+			screenWidth, "center")
+
+		love.graphics.setColor(1, 1, 1, 1)
+		love.graphics.printf(
+			"connect controllers to start", 
+			0, screenHeight / 2.0 + 60, 
+			screenWidth, "center")
+			
+	end
+
+	-- draw each player
 	for jid, player in next, players do 
 		local text1 = player.name
 		local text2 = "position.x:"..player.position.x.." position.y:"..player.position.y
-		local text3 = "cursor.x:"..player.cursor.x.." cursor.y:"..player.cursor.y
 
 		love.graphics.setColor(player.color.r, player.color.g, player.color.b, 1)
-		love.graphics.print(text1, player.cursor.x, player.cursor.y)
-		love.graphics.print(text2, player.cursor.x, player.cursor.y+20)	
-		love.graphics.print(text3, player.cursor.x, player.cursor.y+40)	
+		love.graphics.print(text1, 20, jid * 40 + 0)
+		love.graphics.print(text2, 20, jid * 40 + 20)
 
 	end
 end
@@ -111,4 +135,3 @@ function love.update(dt)
 		end
 	end
 end
-
